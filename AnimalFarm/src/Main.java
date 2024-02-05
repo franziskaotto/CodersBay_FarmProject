f4daf82dabbfebfeb878e4241c35f79b44c1e879
Franziska Otto <franziska.otto@gmx.at>
2024-02-05 11:22:57 +0100

    Added up to task 4

--- AnimalFarm/src/Main.java
+++ AnimalFarm/src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        //2.
+        String animal;
+        int hunger;
+        int hungerSheep;
+        int hungerCow;
+        int hungerChicken;
+        boolean isSheep;
+        boolean isCow;
+        boolean isChicken;
+
+
+        //3.
+        hungerSheep = 50;
+        hungerChicken = 60;
+        hungerCow = 60;
+
+        System.out.println("HungerScore:\n" + "Sheep: " + hungerSheep + "\n" + "Chicken: " + hungerChicken + "\n" + "Cow: " + hungerCow);
+
+
+        //4.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an animal here: ");
+        animal = scanner.next();
+        System.out.println("you entered: " + animal);
+
+
+
     }
+
+
 }
